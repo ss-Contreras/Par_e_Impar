923d78e8bd6ec928d3e56e7b6c19412a2719f27e
xWawi <91096090+xWawi@users.noreply.github.com>
2022-08-26 19:42:56 -0500

    Add files via upload

---
+++ par_impar.py
@@ -0,0 +1,8 @@
+num = input("Introduce un número maximo:")
+num = int(num)
+for i in range (num):
+    if i % 2 == 0:
+        print(f"El numero: {i} es par")
+
+    else:
+        print(f"El numero: {i} es imprar")
